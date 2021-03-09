509a42ea57e93dae15933c714ca7f87861f97073
houseman1 <79094235+houseman1@users.noreply.github.com>
2021-03-09 21:45:11 +0100

    Update Chapitre10 Ex 2.js

--- FRONT/DYNAMIQUE/JAVASCRIPT/Chapitre 10 - les tableaux/Exercice 2/Chapitre10 Ex 2.js
+++ FRONT/DYNAMIQUE/JAVASCRIPT/Chapitre 10 - les tableaux/Exercice 2/Chapitre10 Ex 2.js
@@ -4,6 +4,7 @@ let len = 0;
 let max = 0;
 let av = 0;
 let tot = 0;
+let int = 1;
 
 var btnInit  = document.getElementById("InitTab");//identify button
 btnInit.addEventListener("click", InitTab);//Assign click event 
@@ -28,6 +29,7 @@ btnInfo.addEventListener("click", InfoTab);//Assign click event
 function InitTab(){
     len = window.prompt("Entrez le nombre de postes");
     document.getElementById("nombre").innerHTML = "Le nombre de postes dans votre tableau est " + len + ".";
+    document.getElementById("SaisieTab").disabled = false;
 }
 
 function SaisieTab(){
@@ -36,48 +38,52 @@ function SaisieTab(){
     } else {
         for (i=0; i<=len-1; i++) {
             entry = window.prompt("Entrez un nombre");
+            tot = tot + parseInt(entry);//used to calculate average in InfoTab
+            console.log("tot = " + tot);
             postesArray.push(parseInt(entry));
-            console.log("postesArray = " + postesArray)
-            document.getElementById("nombre").innerHTML = "Le nombre de postes dans votre tableau est " + postesArray.length + ".";
+            console.log("postesArray = " + postesArray);
+            document.getElementById("saisie").innerHTML = "C'est noté !";
         }
-    }   
+    }
+    document.getElementById("AfficheTab").disabled = false;   
 }
 
 function AfficheTab(str){
     str = postesArray.toString();
     document.getElementById("affiche").innerHTML = "Vos postes : " + str;  
     console.log("postesArray = " + str); 
+    document.getElementById("RechercheTab").disabled = false;
 }
 
 function RechercheTab (){
     ndx = window.prompt("Entrez l'index de la valeur que vous cherchez");
     val = postesArray[ndx-1];
     document.getElementById("recherche").innerHTML = "La valuer que vous recherchez est: " + val;
+    document.getElementById("InfoTab").disabled = false;
 }
 
 function InfoTab (){
     for (i=0; i<=len; i++) {
         console.log("i = " + i ) ;
-        tot = tot + postesArray[i]//to calculate total
+
         if (postesArray[i] > max) {
             max = postesArray[i];
             console.log("Max = " + max);
-            //console.log("len = " + len) ;
         }
-        av = tot / parseInt(len);  
     }
+    av = tot / len
     document.getElementById("maximum").innerHTML = "Le maximum des postes est " + max;
     document.getElementById("moyenne").innerHTML = "Le moyenne des postes est " + av;
-
+    document.getElementById("GetIntegerTab").disabled = false;
 }
 
-function GetInteger(int){
+function GetInteger(){
     int = window.prompt("Entrez un nombre");
-    if (Number.isInteger(int)) {
-        document.getElementById("integer").innerHTML = "Votre nombre est un nombre entier.";
+
+    if (int.includes(".")) {
+        document.getElementById("integer").innerHTML = int + " est une 'décimale'.";
     } else {
-        document.getElementById("integer").innerHTML = "Votre nombre est un 'float'.";
+        document.getElementById("integer").innerHTML = int + " est un nombre entier.";    
     }
-    console.log(Number.isInteger("Number.isInteger = " + int));
     console.log(int);
 }
